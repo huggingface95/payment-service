5de9e347eec7e0b9519147742a458925ad342df9
pvsv <pvsv1984@gmail.com>
2021-10-26 21:44:47 +0300

    Fix company settings key

--- api/app/Models/CompanySettings.php
+++ api/app/Models/CompanySettings.php
@@ -16,12 +16,15 @@ class CompanySettings extends Model
     protected $fillable = [
         'email_url', 'email_jwt','email_from','logo_object_key','show_own_logo'
     ];
+
+    public $primaryKey = 'company_id';
     public $timestamps = false;
 
     public function company(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo('App\Models\Companies','company_id');
     }
+
 
 
 }
