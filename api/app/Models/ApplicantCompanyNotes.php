60beedffe7820b9b9ccab59f832b67bbc4a6c47b
pvsv <pvsv1984@gmail.com>
2021-11-24 11:22:20 +0200

    Add notes

---
+++ api/app/Models/ApplicantCompanyNotes.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicantCompanyNotes extends Model
+{
+
+    protected $table="applicant_company_notes";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'note','applicant_company_id','member_id'
+    ];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(Members::class, 'member_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function applicant()
+    {
+        return $this->belongsTo(ApplicantCompany::class, ' applicant_company_id');
+    }
+
+
+}
---
+++ api/app/Models/ApplicantIndividualNotes.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicantIndividualNotes extends Model
+{
+
+    protected $table="applicant_individual_notes";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'note','applicant_individual_id','member_id'
+    ];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(Members::class, 'member_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function applicant()
+    {
+        return $this->belongsTo(ApplicantIndividual::class, ' applicant_individual_id');
+    }
+
+
+}
