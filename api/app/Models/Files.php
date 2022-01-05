cf0c0bbbec5e504dfd52e1684e75598f22a75d61
v.paryacool <v.paryacool@gmail.com>
2022-01-05 23:12:16 +0200

    Add files model

---
+++ api/app/Models/Files.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use http\Env\Request;
+use Illuminate\Database\Eloquent\Model;
+
+class Files extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'file_name', 'mime_type', 'size', 'entity_type', 'author_id', 'storage_path', 'storage_name'
+    ];
+
+    public function store(Request $request)
+    {
+
+    }
+
+}
