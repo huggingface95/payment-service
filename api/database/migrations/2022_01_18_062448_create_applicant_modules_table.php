32530df2543ca22f79e987d6206452bc81e109cc
v.paryacool <v.paryacool@gmail.com>
2022-01-18 19:51:21 +0200

    Added ApplicantIndividualModules

--- api/database/migrations/2022_01_18_062448_create_applicant_modules_table.php
+++ api/database/migrations/2022_01_18_062448_create_applicant_modules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ApplicantModules extends Migration
+class CreateApplicantModulesTable extends Migration
 {
     /**
      * Run the migrations.
