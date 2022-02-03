e482349cfc48e103d65748162c3aad0bbb42c7bd
v.paryacool <v.paryacool@gmail.com>
2022-02-03 12:48:28 +0200

    Add is_active column to applicant labels

---
+++ api/app/GraphQL/Queries/ApplicantIndividualLabelsQuery.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\ApplicantIndividualLabel;
+use GraphQL\Exception\InvalidArgument;
+use Illuminate\Support\Facades\DB;
+
+class ApplicantIndividualLabelsQuery
+{
+    protected $table="applicant_individual_labels";
+
+    public function enabled($_, array $args)
+    {
+        $args = DB::table('applicant_individual_labels as ail')
+            ->select('*',DB::raw('CASE
+                WHEN ailr.applicant_individual_id is not null THEN true
+                ELSE false
+                END AS is_active'))
+            ->leftJoin('applicant_individual_label_relation as ailr','ail.id','=','ailr.applicant_individual_label_id')
+            ->where('ailr.applicant_individual_id','=',$args['where']['value'])
+            ->orWhereNull('ailr.applicant_individual_id')->orderBy('ail.id')
+            ->get();
+        return $args;
+    }
+
+}
