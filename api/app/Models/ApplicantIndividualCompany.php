b9c42367a8720eac5df4d985b0c8ef6eb6f7aa06
v.paryacool <v.paryacool@gmail.com>
2022-02-04 14:54:14 +0200

    Add query for company matched users

--- api/app/Models/ApplicantIndividualCompany.php
+++ api/app/Models/ApplicantIndividualCompany.php
@@ -18,5 +18,24 @@ class ApplicantIndividualCompany extends Model
 
     ];
 
+    public function ApplicantIndividual()
+    {
+        return $this->belongsTo(ApplicantIndividual::class,'applicant_individual_id','id');
+    }
+
+    public function ApplicantIndividualCompanyRelation()
+    {
+        return $this->belongsTo(ApplicantIndividualCompanyRelation::class,'applicant_individual_company_relation_id','id');
+    }
+
+    public function ApplicantIndividualCompanyPosition()
+    {
+        return $this->belongsTo(ApplicantIndividualCompanyPosition::class,'applicant_individual_company_position_id','id');
+    }
+
+    public function ApplicantIndividualState()
+    {
+        return $this->belongsTo(ApplicantStatus::class,'applicant_individual_status_id','id');
+    }
 
 }
