91bdc39bcaacd4e0de8ebfa3aa35ca0a8ccc016d
pvsv <pvsv1984@gmail.com>
2022-02-08 22:30:13 +0200

    Fix select applicant labels

--- api/app/GraphQL/Queries/ApplicantIndividualLabelsQuery.php
+++ api/app/GraphQL/Queries/ApplicantIndividualLabelsQuery.php
@@ -13,12 +13,11 @@ class ApplicantIndividualLabelsQuery
     {
         $args = DB::table('applicant_individual_labels as ail')
             ->select('*',DB::raw('CASE
-                WHEN ailr.applicant_individual_id is not null THEN true
+                WHEN ailr.applicant_individual_id is not null and ailr.applicant_individual_id = '.$args['applicant_id'].' THEN true
+                WHEN ailr.applicant_individual_id = '.$args['applicant_id'].'  THEN true
                 ELSE false
                 END AS is_active'))
             ->leftJoin('applicant_individual_label_relation as ailr','ail.id','=','ailr.applicant_individual_label_id')
-            ->where('ailr.applicant_individual_id','=',$args['applicant_id'])
-            ->orWhereNull('ailr.applicant_individual_id')->orderBy('ail.id')
             ->get();
         return $args;
     }
