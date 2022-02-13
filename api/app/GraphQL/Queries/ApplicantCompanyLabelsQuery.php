3f34117e23234bb9975352bec12ff5e5eff6fc3d
v.paryacool <v.paryacool@gmail.com>
2022-02-13 17:49:35 +0200

    Fix applicant company query

--- api/app/GraphQL/Queries/ApplicantCompanyLabelsQuery.php
+++ api/app/GraphQL/Queries/ApplicantCompanyLabelsQuery.php
@@ -13,8 +13,8 @@ class ApplicantCompanyLabelsQuery
     {
         $args = DB::table('applicant_company_labels as ail')
             ->select('*',DB::raw('CASE
-                WHEN ailr.applicant_company_id is not null and ailr.applicant_company_id = '.$args['applicant_id'].' THEN true
-                WHEN ailr.applicant_company_id = '.$args['applicant_id'].'  THEN true
+                WHEN ailr.applicant_company_id is not null and ailr.applicant_company_id = '.$args['company_id'].' THEN true
+                WHEN ailr.applicant_company_id = '.$args['company_id'].'  THEN true
                 ELSE false
                 END AS is_active'))
             ->leftJoin('applicant_company_label_relation as ailr','ail.id','=','ailr.applicant_company_label_id')
