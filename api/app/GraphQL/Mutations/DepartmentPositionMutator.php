062cd63031b850f3ff4f2d9da9c80c9d1318eb7d
pvsv <pvsv1984@gmail.com>
2022-02-14 11:39:35 +0200

    Fix department position

--- api/app/GraphQL/Mutations/DepartmentPositionMutator.php
+++ api/app/GraphQL/Mutations/DepartmentPositionMutator.php
@@ -4,11 +4,36 @@ namespace App\GraphQL\Mutations;
 
 use App\Exceptions\GraphqlException;
 use App\Models\DepartmentPosition;
+use App\Models\Departments;
 
 
 class DepartmentPositionMutator extends BaseMutator
 {
 
+
+    /**
+     * Update position
+     * @param $root
+     * @param array $args
+     * @return mixed
+     */
+    public function create($root, array $args)
+    {
+
+        if ($args['department_id']) {
+            $department = Departments::find($args[$args['department_id']]);
+            unset($args['department_id']);
+            $position = DepartmentPosition::create($args);
+            if (!$department) {
+                throw new GraphqlException('Department not found',"not found", 404);
+            }
+            $department->attach($position->id);
+        } else {
+            $position = DepartmentPosition::create($args);
+        }
+
+        return $position;
+    }
 
     /**
      * Update position
@@ -22,7 +47,7 @@ class DepartmentPositionMutator extends BaseMutator
             if (!$position->is_active && !$position->members->isEmpty() ) {
                 throw new GraphqlException('Department positions are already in use',"used");
             }
-            $position->update();
+            $position->update($args);
 
             return $position;
     }
