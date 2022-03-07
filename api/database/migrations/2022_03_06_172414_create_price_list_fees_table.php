3fd9e41b3fbc3079b4ca6f7770d1271b6c870bfc
artur <arthurgevorgyan1992@gmail.com>
2022-03-07 22:41:40 +0400

    Add price list fee section

---
+++ api/database/migrations/2022_03_06_172414_create_price_list_fees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePriceListFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('price_list_fees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('price_list_id');
+            $table->tinyInteger('type')->default(0);
+            $table->tinyInteger('operation_type')->default(0);
+            $table->integer('period')->default(0);
+            $table->json('fee')->nullable();
+            $table->timestamps();
+
+            $table->foreign('price_list_id')->references('id')->on('commission_price_list');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('price_list_fees');
+    }
+}
