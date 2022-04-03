c59f06c87915d0bf2d47b07aaf34ea3cedf30a9d
pvsv <pvsv1984@gmail.com>
2022-04-03 22:44:50 +0300

    Add group relation to account and fix account table fields

--- api/database/migrations/2021_12_18_101921_create_accounts_table.php
+++ api/database/migrations/2021_12_18_101921_create_accounts_table.php
@@ -22,12 +22,12 @@ class CreateAccountsTable extends Migration
             $table->enum('account_type', ['Business', 'Private']);
             $table->unsignedBigInteger('payment_provider_id');
             $table->unsignedBigInteger('commission_template_id');
-            $table->string('account_state', 255);
+            $table->unsignedBigInteger('account_state', 255);
             $table->string('account_name', 255)->unique();
             $table->boolean('is_primary');
-            $table->bigInteger('current_balance');
-            $table->bigInteger('reserved_balance');
-            $table->bigInteger('available_balance');
+            $table->decimal('current_balance',15,5)->default(0);
+            $table->decimal('reserved_balance',15,5)->default(0);
+            $table->decimal('available_balance',15,5)->default(0);
             $table->timestamps();
             $table->timestamp('activated_at')->nullable();
             $table->foreign('currency_id')->references('id')->on('currencies');
@@ -35,7 +35,7 @@ class CreateAccountsTable extends Migration
             $table->foreign('owner_id')->references('id')->on('members');
             $table->foreign('payment_provider_id')->references('id')->on('payment_provider');
             $table->foreign('commission_template_id')->references('id')->on('commission_template');
-            $table->foreign('account_state')->references('name')->on('account_states');
+            $table->foreign('account_state')->references('id')->on('account_states');
         });
     }
 
