30bbf1f5c48e54efa45d0de0a8ecd2b6cc054fce
pvsv <pvsv1984@gmail.com>
2022-04-03 12:47:17 +0300

    Add member_group_role_id to applicant company

---
+++ api/database/migrations/2022_04_02_084344_add_group_id_applicant_company_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddGroupIdApplicantCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applicant_company', function (Blueprint $table) {
+            $table->unsignedBigInteger('member_group_role_id')->nullable();
+            $table->foreign('member_group_role_id')->references('id')->on('group_role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applicant_individual', function (Blueprint $table) {
+            $table->dropColumn('member_group_role_id');
+        });
+    }
+}
