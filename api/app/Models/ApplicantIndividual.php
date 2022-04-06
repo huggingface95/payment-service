ca1e1b56cc454d939de7a407bc71a850a7179788
pvsv <pvsv1984@gmail.com>
2022-04-06 17:22:46 +0300

    Add order by group name

--- api/app/Models/ApplicantIndividual.php
+++ api/app/Models/ApplicantIndividual.php
@@ -181,4 +181,9 @@ class ApplicantIndividual extends Model
         return $this->belongsTo(GroupRole::class,'member_group_role_id');
     }
 
+    public function scopeGroupSort($query, $sort)
+    {
+        return $query->join('group_role','group_role.id','=','applicant_individual.member_group_role_id')->orderBy('group_role.name',$sort)->select('applicant_individual.*');
+    }
+
 }
