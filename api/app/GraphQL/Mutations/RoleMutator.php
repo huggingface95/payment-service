c57366676a7dab9817524f6d9a575c17d22b11af
pvsv <pvsv1984@gmail.com>
2022-04-11 21:55:51 +0300

    fix role groups

--- api/app/GraphQL/Mutations/RoleMutator.php
+++ api/app/GraphQL/Mutations/RoleMutator.php
@@ -74,7 +74,7 @@ class RoleMutator
         $currentGroups = $role->getGroupsIdByRole();
         $groupsDelete = array_diff($currentGroups,$groups);
         if ($groupsDelete) {
-            GroupRole::where('role_id',$role->id)->whereIn('id',$groupsDelete)->delete();
+            GroupRole::whereIn('id',$groupsDelete)->delete();
         }
         foreach ($groups as $group) {
             GroupRole::updateOrCreate(['role_id'=>$role->id, 'id'=>$group],['id'=> $group]);
