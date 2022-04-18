f24ab17038e1eba3f965bed02686968f13efe5b5
pvsv <pvsv1984@gmail.com>
2022-04-18 13:46:11 +0300

    Change permissions table

--- api/database/migrations/2022_04_15_124310_add_fields_to_permissions_table.php
+++ api/database/migrations/2022_04_15_124310_add_fields_to_permissions_table.php
@@ -14,9 +14,9 @@ class AddFieldsToPermissionsTable extends Migration
     public function up()
     {
         Schema::table('permissions', function (Blueprint $table) {
-            $table->unsignedBigInteger('permission_group_id')->nullable();
+            $table->unsignedBigInteger('permission_list_id')->nullable();
             $table->enum('type',['info','export','edit','add','important'])->default('info');
-            $table->foreign('permission_group_id')->references('id')->on('permission_category');
+            $table->foreign('permission_list_id')->references('id')->on('permissions_list');
         });
     }
 
