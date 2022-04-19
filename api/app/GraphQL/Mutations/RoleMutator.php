342494cba4329cdd20aa326c20f796e757068e4b
pvsv <pvsv1984@gmail.com>
2022-04-19 10:10:04 +0300

    Fix role create

--- api/app/GraphQL/Mutations/RoleMutator.php
+++ api/app/GraphQL/Mutations/RoleMutator.php
@@ -18,7 +18,7 @@ class RoleMutator
      */
     public function create($_, array $args)
     {
-        if ($args['groups'][0] == '2') {
+        if (isset($args['groups']) && $args['groups'][0] == '2') {
             throw new GraphqlException('Role is not be used for this group',"internal", 500);
         }
 
