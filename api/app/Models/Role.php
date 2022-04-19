0e1883bcb7c307b3da1922094647786172d26968
pvsv <pvsv1984@gmail.com>
2022-04-19 10:30:08 +0300

    Fix roles

--- api/app/Models/Role.php
+++ api/app/Models/Role.php
@@ -9,7 +9,7 @@ use Spatie\Permission\Models\Role as SpatieRole;
 class Role extends SpatieRole
 {
     protected $fillable = [
-        'name', 'guard_name', 'description'
+        'name', 'guard_name', 'description','company_id','group_type_id'
     ];
 
     protected $guard_name = GuardEnum::GUARD_NAME;
