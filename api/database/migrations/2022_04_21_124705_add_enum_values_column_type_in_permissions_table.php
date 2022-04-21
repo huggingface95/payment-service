11b15e87e0e6196e86bbe132343100baf02f8351
artur <arthurgevorgyan1992@gmail.com>
2022-04-21 20:57:07 +0400

    Add member type permissions

---
+++ api/database/migrations/2022_04_21_124705_add_enum_values_column_type_in_permissions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Permissions;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AddEnumValuesColumnTypeInPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE permissions DROP CONSTRAINT permissions_type_check");
+        $types = [
+            Permissions::TYPE_ADD, Permissions::TYPE_EDIT, Permissions::TYPE_READ,
+            Permissions::TYPE_INFO, Permissions::TYPE_IMPORTANT,
+            Permissions::TYPE_REQUIRED, Permissions::TYPE_NO_REQUIRED, Permissions::TYPE_EXPORT
+        ];
+        $result = join( ', ', array_map(function ($value){
+            return sprintf("'%s'::character varying", $value);
+        }, $types));
+        DB::statement("ALTER TABLE permissions ADD CONSTRAINT permissions CHECK (type::text = ANY (ARRAY[$result]::text[]))");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
