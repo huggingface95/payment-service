f75a7d3b2dee56bef23ecb2310fe3d400c88a363
artur <arthurgevorgyan1992@gmail.com>
2022-04-25 16:28:28 +0400

    Change name unique in group_role table

---
+++ api/database/migrations/2022_04_25_122408_change_unique_name_to_group_role_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeUniqueNameToGroupRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('group_role', function (Blueprint $table) {
+            $table->dropUnique('group_role_un');
+            $table->unique(['name', 'group_type_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('group_role', function (Blueprint $table) {
+            //
+        });
+    }
+}
