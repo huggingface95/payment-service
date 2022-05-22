f71eeca5a628ad391b981bbc374546021409f161
artur <arthurgevorgyan1992@gmail.com>
2022-05-23 01:20:11 +0400

    Optimize iban functionality part go

--- cl-junc-api/cmd/services/web/api/postback.go
+++ cl-junc-api/cmd/services/web/api/postback.go
@@ -97,8 +97,10 @@ func IbanPostback(c *gin.Context) {
 
 	if response.Status == models2.Accepted {
 		app.Get.UpdateAccount(&db.Account{
-			AccountState: db.GetAccountState(response.Status),
-		}, "payment_number", "account_state")
+			AccountNumber: response.OrderReference,
+			Iban:          response.Iban,
+			AccountState:  db.GetAccountState(response.Status),
+		}, "account_number", "account_state", "iban")
 
 		c.Data(200, "text/plain", []byte(response.OrderReference))
 	}
