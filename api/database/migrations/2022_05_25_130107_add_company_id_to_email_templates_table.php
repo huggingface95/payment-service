4f7595f0a8203324401aa8ff7bf0b17a5039acbb
pvsv <pvsv1984@gmail.com>
2022-05-26 15:46:01 +0300

    Fix email templates migration

--- api/database/migrations/2022_05_25_130107_add_company_id_to_email_templates_table.php
+++ api/database/migrations/2022_05_25_130107_add_company_id_to_email_templates_table.php
@@ -14,7 +14,7 @@ class AddCompanyIdToEmailTemplatesTable extends Migration
     public function up()
     {
         Schema::table('email_templates', function (Blueprint $table) {
-            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('company_id')->nullable();
 
             $table->foreign('company_id')->references('id')->on('companies')
                 ->onDelete('cascade')
