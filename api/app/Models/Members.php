8aae55153643f154fd4f1fbce1dd1c9bba8da9bd
pvsv <pvsv1984@gmail.com>
2022-05-31 11:07:16 +0300

    Add is_sign_transaction field to member

--- api/app/Models/Members.php
+++ api/app/Models/Members.php
@@ -38,7 +38,7 @@ class Members extends BaseModel implements AuthenticatableContract, Authorizable
     public $password_confirmation;
 
     protected $fillable = [
-        'first_name', 'last_name', 'email', 'sex', 'is_active', 'company_id', 'country_id', 'language_id', 'two_factor_auth_setting_id', 'password_hash', 'password_salt', 'last_login_at', 'additional_fields', 'additional_info_fields', 'is_show_owner_applicants'
+        'first_name', 'last_name', 'email', 'sex', 'is_active', 'company_id', 'country_id', 'language_id', 'two_factor_auth_setting_id', 'password_hash', 'password_salt', 'last_login_at', 'additional_fields', 'additional_info_fields', 'is_show_owner_applicants','is_sign_transaction'
     ];
 
     protected $hidden = [
