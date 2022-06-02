1ab171a4470e59be677debdda06837ed03ee15c7
artur <arthurgevorgyan1992@gmail.com>
2022-06-02 19:41:36 +0400

    Optimize email templates

---
+++ api/database/migrations/2022_06_02_153139_optimize_columns_to_email_templates_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\EmailTemplate;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class OptimizeColumnsToEmailTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE email_templates DROP CONSTRAINT email_templates_service_type_check");
+
+        $types = EmailTemplate::getServiceTypes();
+        $result = join( ', ', array_map(function ($value){
+            return sprintf("'%s'::character varying", $value);
+        }, $types));
+
+        DB::statement("ALTER TABLE email_templates ADD CONSTRAINT email_templates_service_type_check CHECK (service_type::text = ANY (ARRAY[$result]::text[]))");
+
+
+        Schema::table('email_templates', function (Blueprint $table) {
+            $table->string('header')->nullable()->change();
+            $table->string('footer')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('email_templates', function (Blueprint $table) {
+            //
+        });
+    }
+}
