befe947633cafccef1b1033569c776737128329f
pvsv <pvsv1984@gmail.com>
2022-06-13 18:15:24 +0300

    Add unique email smtp table

---
+++ api/database/migrations/2022_06_13_151229_add_unique_field_smtp.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUniqueFieldSmtp extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('email_smtps', function (Blueprint $table) {
+            $table->unique(['name','company_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
