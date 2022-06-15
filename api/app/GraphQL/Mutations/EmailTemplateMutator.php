217e0fff694fa19ba62196b45a7053f80782d34f
artur <arthurgevorgyan1992@gmail.com>
2022-06-15 15:13:28 +0400

    Add sendEmailWithData

--- api/app/GraphQL/Mutations/EmailTemplateMutator.php
+++ api/app/GraphQL/Mutations/EmailTemplateMutator.php
@@ -5,6 +5,7 @@ namespace App\GraphQL\Mutations;
 use App\DTO\Email\SmtpConfigDTO;
 use App\DTO\Email\SmtpDataDTO;
 use App\DTO\TransformerDTO;
+use App\Exceptions\GraphqlException;
 use App\Jobs\SendMailJob;
 use App\Models\BaseModel;
 use App\Models\EmailSmtp;
@@ -21,17 +22,26 @@ class EmailTemplateMutator
         return EmailTemplate::create($args);
     }
 
-    public function sendEmailWithData($root, array $args){
-        /** @var Members $member */
-        $member = Auth::user();
-        /** @var EmailSmtp $smtp */
-        $smtp = EmailSmtp::where('member_id', $member->id)->where('company_id', $args['company_id'])->first();
-        $smtp->replay_to = $args['email'];
-        $data = TransformerDTO::transform(SmtpDataDTO::class, $smtp, $args['content'] ?? '', $args['subject']);
-        $config = TransformerDTO::transform(SmtpConfigDTO::class, $smtp);
-        dispatch(new SendMailJob($config, $data));
+    public function sendEmailWithData($root, array $args): array
+    {
 
-        return ['status'=>'OK', "message"=>"Email sent for processing"];
+        try {
+            /** @var Members $member */
+            $member = Auth::user();
+
+            /** @var EmailSmtp $smtp */
+            $smtp = EmailSmtp::where('member_id', $member->id)->where('company_id', $args['company_id'])->first();
+            $smtp->replay_to = $args['email'];
+            $data = TransformerDTO::transform(SmtpDataDTO::class, $smtp, $args['content'] ?? '', $args['subject']);
+            $config = TransformerDTO::transform(SmtpConfigDTO::class, $smtp);
+            dispatch(new SendMailJob($config, $data));
+
+            return ['status'=>'OK', "message"=>"Email sent for processing"];
+        }
+        catch (\Throwable $e){
+            throw new GraphqlException($e->getMessage(),"Internal",$e->getCode());
+        }
+
     }
 
 }
