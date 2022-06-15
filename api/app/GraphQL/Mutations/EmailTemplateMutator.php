bdb8ee2c297bc91b17547666cba3ee7130d78dc5
artur <arthurgevorgyan1992@gmail.com>
2022-06-15 14:58:19 +0400

    Add sendEmailWithData

--- api/app/GraphQL/Mutations/EmailTemplateMutator.php
+++ api/app/GraphQL/Mutations/EmailTemplateMutator.php
@@ -2,8 +2,15 @@
 
 namespace App\GraphQL\Mutations;
 
+use App\DTO\Email\SmtpConfigDTO;
+use App\DTO\Email\SmtpDataDTO;
+use App\DTO\TransformerDTO;
+use App\Jobs\SendMailJob;
 use App\Models\BaseModel;
+use App\Models\EmailSmtp;
 use App\Models\EmailTemplate;
+use App\Models\Members;
+use Illuminate\Support\Facades\Auth;
 
 class EmailTemplateMutator
 {
@@ -13,4 +20,16 @@ class EmailTemplateMutator
 
         return EmailTemplate::create($args);
     }
+
+    public function sendEmailWithData($root, array $args){
+        /** @var Members $member */
+        $member = Auth::user();
+        /** @var EmailSmtp $smtp */
+        $smtp = EmailSmtp::where('member_id', $member->id)->where('company_id', $args['company_id'])->first();
+        $smtp->replay_to = $args['email'];
+        $data = TransformerDTO::transform(SmtpDataDTO::class, $smtp, $args['content'] ?? '', $args['subject']);
+        $config = TransformerDTO::transform(SmtpConfigDTO::class, $smtp);
+        dispatch(new SendMailJob($config, $data));
+    }
+
 }
