b172f96cd84ed0b514b3f84cb4ef6bb5ca24f09c
p.vladimir <v.paryacool@gmail.com>
2022-06-21 09:47:36 +0300

    Fix auth by ip

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -38,10 +38,7 @@ class AuthController extends Controller
 
         $user = auth()->user();
         $get_ip_address = $user->ipAddress()->pluck('ip_address')->toArray();
-        $f = fopen('data.txt', 'w');
-        fwrite($f, json_encode($get_ip_address));
-        fclose($f);
-        if (!$get_ip_address) {
+        if ($get_ip_address) {
             if(!in_array(request()->ip(), $get_ip_address)){
                 return response()->json(['error' => 'Access denied'], 403);
             }
