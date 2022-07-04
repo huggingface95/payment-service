5a6a966e4a4e76018cede83b7586d1fd3b293981
pvsv <pvsv1984@gmail.com>
2022-07-04 19:51:15 +0300

    Add group mode

--- api/app/GraphQL/Queries/GroupsQuery.php
+++ api/app/GraphQL/Queries/GroupsQuery.php
@@ -2,13 +2,21 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Models\GroupRole;
+use App\Models\Groups;
+
 class GroupsQuery
 {
     /**
      * @param  null  $_
      * @param  array<string, mixed>  $args
      */
-    public function __invoke($_, array $args)
+    public function get($_, array $args)
     {
+        if (isset($args['mode']) && $args['mode'] === 'clients') {
+            return Groups::where('id','!=',GroupRole::MEMBER)->get();
+        } else {
+            return Groups::get();
+        }
     }
 }
