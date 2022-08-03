6dd95e34a4316a69f8e3a3f3214b7124d24de7bf
p.vladimir <v.paryacool@gmail.com>
2022-08-03 13:57:06 +0300

    Fix login with 2fa

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -301,6 +301,7 @@ class AuthController extends Controller
             ];
             $user->save();
             if ($data == true) {
+                $token = JWTAuth::fromUser($user);
                 return response()->json(['data' => 'success', 'token' => $token]);
             } else {
                 return response()->json(['error' => 'No such code'], 403);
