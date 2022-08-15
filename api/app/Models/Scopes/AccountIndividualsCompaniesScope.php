6883d2ede3c85f76ad0783a986a32dfcd6730d3b
artur <arthurgevorgyan1992@gmail.com>
2022-08-15 17:38:12 +0400

    Bag fix

--- api/app/Models/Scopes/AccountIndividualsCompaniesScope.php
+++ api/app/Models/Scopes/AccountIndividualsCompaniesScope.php
@@ -9,11 +9,15 @@ use Illuminate\Support\Facades\DB;
 
 class AccountIndividualsCompaniesScope implements Scope
 {
-    public function apply(Builder $builder, Model $model)
+    public function apply(Builder $builder, Model $model): Builder
     {
+        $request = request()->request->all();
+        if (array_key_exists('operationName', $request) && in_array($request['operationName'], ['UpdateAccount', 'CreateAccount'])){
+            return $builder;
+        }
         return $builder->fromSub(
             DB::table('accounts')->leftJoin('account_individuals_companies', 'account_individuals_companies.account_id', '=', 'accounts.id')
-            ->select(['accounts.*', 'account_individuals_companies.client_id', 'account_individuals_companies.client_type']),
+                ->select(['accounts.*', 'account_individuals_companies.client_id', 'account_individuals_companies.client_type']),
             'accounts'
         );
     }
