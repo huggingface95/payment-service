79ac0f661770abb16c55bc751304729fed3c967f
artur <arthurgevorgyan1992@gmail.com>
2022-08-19 15:07:00 +0400

    Bag fix

--- api/app/DTO/GraphQLResponse/EmailTemplateOnCompanyResponse.php
+++ api/app/DTO/GraphQLResponse/EmailTemplateOnCompanyResponse.php
@@ -4,12 +4,13 @@ namespace App\DTO\GraphQLResponse;
 
 use App\Models\EmailTemplate;
 use App\Models\EmailTemplateLayout;
+use Illuminate\Support\Collection;
 
 class EmailTemplateOnCompanyResponse
 {
     public ?EmailTemplateLayout $layout;
 
-    public array $data;
+    public Collection $data;
 
     public static function transform(EmailTemplate $emailTemplate): self
     {
@@ -18,7 +19,7 @@ class EmailTemplateOnCompanyResponse
         $dto->data = EmailTemplate::query()
             ->where('company_id', $emailTemplate->company_id)
             ->where('type', $emailTemplate->type)
-            ->get()->toArray();
+            ->get();
         return $dto;
     }
 }
