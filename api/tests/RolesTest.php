c3f885356343f0487e524e7609ec2b3403cbc40d
p.vladimir <v.paryacool@gmail.com>
2022-08-31 13:47:13 +0300

    Fix roles filter and tests

--- api/tests/RolesTest.php
+++ api/tests/RolesTest.php
@@ -126,7 +126,63 @@ class RolesTest extends TestCase
 
         ($this->graphQL('
             query {
-                roles(hasGroupType:{column:ID  , value: 1 }) {
+                roles(filter: { column: HAS_GROUP_TYPE_MIXED_ID_OR_NAME, value: 1 }) {
+                    data {
+                        id
+                        name
+                    }
+                }
+            }
+        '))->seeJson([
+            'data' => [
+                'roles' => [
+                    'data' => [[
+                        'id' => strval($role[0]->id),
+                        'name' => strval($role[0]->name),
+                    ]],
+                ],
+            ],
+        ]);
+    }
+
+    public function testQueryByCompanyId(): void
+    {
+        $this->login();
+
+        $role= DB::connection('pgsql_test')
+            ->table('roles')
+            ->orderBy('id', 'DESC')
+            ->take(1)
+            ->get();
+
+        ($this->graphQL('
+            query {
+                roles(filter: { column: COMPANY_ID, value: 1 }) {
+                    data {
+                        id
+                        name
+                    }
+                }
+            }
+        '))->seeJsonContains([
+           'id' => strval($role[0]->id),
+           'name' => strval($role[0]->name),
+        ]);
+    }
+
+    public function testQueryByRoleName(): void
+    {
+        $this->login();
+
+        $role= DB::connection('pgsql_test')
+            ->table('roles')
+            ->orderBy('id', 'ASC')
+            ->take(1)
+            ->get();
+
+        ($this->graphQL('
+            query Roles($name: Mixed) {
+                roles(filter: { column: NAME, operator: ILIKE, value: $name }) {
                     data {
                         id
                         name
@@ -134,7 +190,7 @@ class RolesTest extends TestCase
                 }
             }
         ', [
-            'id' => strval($role[0]->id),
+            'name' => 'Super Role',
         ]))->seeJson([
             'data' => [
                 'roles' => [
