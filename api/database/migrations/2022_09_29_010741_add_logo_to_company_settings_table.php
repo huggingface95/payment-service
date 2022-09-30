ebd82ce8aa69c014c85dcc9c901927bf20df06c3
pvsv <pvsv1984@gmail.com>
2022-09-30 11:32:03 +0300

    Fix migration

--- api/database/migrations/2022_09_29_010741_add_logo_to_company_settings_table.php
+++ api/database/migrations/2022_09_29_010741_add_logo_to_company_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddLogoToPaymentProviderTable extends Migration
+class AddLogoToCompanySettingsTable extends Migration
 {
     /**
      * Run the migrations.
