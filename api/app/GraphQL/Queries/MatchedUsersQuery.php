d3c066317ce6643de941f541d17f5e9658558c5d
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2022-11-15 19:33:15 +0100

    Ability to add applicant companies and applicant individuals to applicant companies, added percentage_owned filed and filter by applicant_type

---
+++ api/app/GraphQL/Queries/MatchedUsersQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\ApplicantIndividualCompany;
+
+class MatchedUsersQuery
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function getList($_, array $args)
+    {
+        $applicantIndividualCompanies = ApplicantIndividualCompany::where('applicant_company_id', $args['applicant_company_id']);
+
+        if (isset($args['filter']['column']) && $args['filter']['column'] === 'applicant_type') {
+            $applicantIndividualCompanies->where('applicant_type', 'App\\Models\\' . $args['filter']['value'])->get();
+        }
+
+        return $applicantIndividualCompanies->orderBy('applicant_id', 'ASC')->get();
+    }
+}
