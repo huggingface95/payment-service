6fd40c43126ea2792753d19d594f7dbe360658e0
pvsv <pvsv1984@gmail.com>
2022-12-08 10:52:27 +0200

    Change payment statuses

--- api/app/Enums/PaymentStatusEnum.php
+++ api/app/Enums/PaymentStatusEnum.php
@@ -5,23 +5,23 @@ namespace App\Enums;
 enum PaymentStatusEnum: int
 {
     case PENDING = 1;
-    case COMPLETED = 2;
+    case SENT = 2;
     case ERROR = 3;
     case CANCELED = 4;
     case UNSIGNED = 5;
     case WAITING_EXECUTION_DATE = 6;
-    case EXECUTION = 7;
+    case EXECUTED = 7;
 
     public function toString(): string
     {
         return match ($this) {
             self::PENDING => 'Pending',
-            self::COMPLETED => 'Completed',
+            self::SENT => 'Sent',
             self::ERROR => 'Error',
             self::CANCELED => 'Canceled',
             self::UNSIGNED => 'Unsigned',
             self::WAITING_EXECUTION_DATE => 'Waiting execution date',
-            self::EXECUTION => 'Execution',
+            self::EXECUTED => 'Executed',
         };
     }
 }
