aab05b2cbe5bde3eac26ed600e6a1f042d84a050
p.vladimir <v.paryacool@gmail.com>
2022-12-27 16:50:12 +0100

    Added reset password send email to Applicant

--- api/app/Http/Controllers/PasswordController.php
+++ api/app/Http/Controllers/PasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\DTO\Email\Request\EmailApplicantRequestDTO;
 use App\DTO\Email\Request\EmailMembersRequestDTO;
 use App\DTO\TransformerDTO;
 use App\Enums\MemberStatusEnum;
@@ -12,6 +13,7 @@ use App\Traits\ResetsPasswords;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Services\AuthService;
 
 class PasswordController extends Controller
 {
@@ -56,7 +58,7 @@ class PasswordController extends Controller
 
         if ($member->update($data)) {
             $company = $member->company;
-            
+
             $emailTemplateName = 'Successful Password Reset';
             $emailData = [
                 'email' => $member->email,
@@ -121,6 +123,48 @@ class PasswordController extends Controller
 
             $emailDTO = TransformerDTO::transform(EmailMembersRequestDTO::class, $user, $emailData, $emailTemplateName);
             $this->emailService->sendMemberEmailByMemberDto($emailDTO, true);
+
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json(['success' => false]);
+    }
+
+    public function resetApplicantPassword(Request $request): JsonResponse
+    {
+        $rules = $this->getResetValidationRulesByToken();
+        unset($rules['token']);
+
+        $this->validate($request, $rules);
+
+        // One-time link valid 24 hours
+        $user = $this->verifyService->getVerifyUserModelByToken($request->token, env('EMAIL_VERIFY_TOKEN_TTL', 24));
+        $this->verifyService->deleteVerifyCode($request->token);
+
+        if (! $user) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Applicant not found by token or token is incorrect',
+            ]);
+        }
+        $generate = new AuthService();
+        $newPassword = $generate->generateUniqueCode() . rand(1, 99);
+
+        $data['password_hash'] = Hash::make($newPassword);
+        $data['password_salt'] = $data['password_hash'];
+
+        if ($user->update($data)) {
+            $company = $user->company;
+            $emailTemplateName = 'Successful Password Reset';
+            $emailData = [
+                'email' => $user->email,
+                'member_email' => $user->email,
+                'member_name' => $user->first_name,
+                'password' => $newPassword,
+            ];
+
+            $emailDTO = TransformerDTO::transform(EmailApplicantRequestDTO::class, $user, $company, $emailTemplateName, $emailData);
+            $this->emailService->sendApplicantEmailByApplicantDto($emailDTO);
 
             return response()->json(['success' => true]);
         }
