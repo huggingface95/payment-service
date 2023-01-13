12ec49821d1eb4c10d325b9d710b8cbea7c8fa0e
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2023-01-13 12:10:44 +0100

    Changed applicant document tags to applicant document internal note tags

--- api/app/GraphQL/Mutations/ApplicantDocumentInternalNoteMutator.php
+++ api/app/GraphQL/Mutations/ApplicantDocumentInternalNoteMutator.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Mutations;
 
+use App\Exceptions\GraphqlException;
 use App\Models\ApplicantDocumentInternalNote;
 
 class ApplicantDocumentInternalNoteMutator
@@ -17,5 +18,30 @@ class ApplicantDocumentInternalNoteMutator
         $args['member_id'] = $user->id;
 
         return ApplicantDocumentInternalNote::create($args);
+    }
+
+    public function attachTag($root, array $args): ApplicantDocumentInternalNote
+    {
+        $note = ApplicantDocumentInternalNote::find($args['applicant_document_internal_note_id']);
+        if (!$note) {
+            throw new GraphqlException('Applicant document internal note not found', 'not found', 404);
+        }
+
+        $note->tags()->detach();
+        $note->tags()->attach($args['applicant_document_tag_id']);
+
+        return $note;
+    }
+
+    public function detachTag($root, array $args): ApplicantDocumentInternalNote
+    {
+        $note = ApplicantDocumentInternalNote::find($args['applicant_document_internal_note_id']);
+        if (!$note) {
+            throw new GraphqlException('Applicant document internal note not found', 'not found', 404);
+        }
+
+        $note->tags()->detach($args['applicant_document_tag_id']);
+
+        return $note;
     }
 }
