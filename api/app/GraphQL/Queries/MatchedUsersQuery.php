6ee4204311f66c4f5b63a2b00fd8078a44f2fa08
p.vladimir <v.paryacool@gmail.com>
2023-01-17 13:22:33 +0100

    Added getMatchedApplicantndividuals

--- api/app/GraphQL/Queries/MatchedUsersQuery.php
+++ api/app/GraphQL/Queries/MatchedUsersQuery.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Models\ApplicantBankingAccess;
 use App\Models\ApplicantIndividualCompany;
 
 class MatchedUsersQuery
@@ -19,5 +20,21 @@ class MatchedUsersQuery
         }
 
         return $applicantIndividualCompanies->orderBy('applicant_id', 'ASC')->get();
+    }
+
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function getApplicantIndividuals($_, array $args)
+    {
+        $applicantIndividualCompanies = ApplicantIndividualCompany::query();
+        $applicantBankingAccess = ApplicantBankingAccess::query()->pluck('applicant_individual_id')->toArray();
+
+        if (isset($args['applicant_company_id'])) {
+            $applicantIndividualCompanies->where('applicant_company_id', $args['applicant_company_id']);
+        }
+
+        return $applicantIndividualCompanies->whereNotIn('applicant_id', $applicantBankingAccess)->where('applicant_type', 'ApplicantIndividual')->orderBy('applicant_id', 'ASC')->get();
     }
 }
