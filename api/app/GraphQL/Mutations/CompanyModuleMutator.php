dfe02f5a95669bc340ec41102eb60dc5d152de9e
pvsv <pvsv1984@gmail.com>
2023-01-17 22:21:36 +0200

    fix company module attach

--- api/app/GraphQL/Mutations/CompanyModuleMutator.php
+++ api/app/GraphQL/Mutations/CompanyModuleMutator.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Mutations;
 
+use App\Exceptions\GraphqlException;
 use App\Models\Company;
 use App\Models\CompanyModule;
 
@@ -11,14 +12,16 @@ class CompanyModuleMutator extends BaseMutator
     public function attach($root, array $args): Company
     {
         $company = Company::find($args['company_id']);
+        if (!$company) {
+            throw new GraphqlException('Company does not exist', 'not found', 404);
+        }
 
         if (isset($args['module_id'])) {
-            $company->modules()->delete();
+            CompanyModule::where('company_id',$args['company_id'])->delete();
             foreach ($args['module_id'] as $module) {
-                CompanyModule::insert([
-                    'module_id' => $module,
-                    'company_id' => $args['company_id'],
-                    'is_active' => $args['is_active'] ?? false,
+                CompanyModule::create([
+                   'company_id' =>  $args['company_id'],
+                    'module_id' => $module->id
                 ]);
             }
         }
@@ -34,21 +37,4 @@ class CompanyModuleMutator extends BaseMutator
         return $company;
     }
 
-    public function update($root, array $args): Company
-    {
-        $company = Company::find($args['company_id']);
-
-        if (isset($args['module_id'])) {
-            foreach ($args['module_id'] as $module) {
-                CompanyModule::where([
-                    'company_id' => $args['company_id'],
-                    'module_id' => $module,
-                ])->update([
-                    'is_active' => $args['is_active'],
-                ]);
-            }
-        }
-
-        return $company;
-    }
 }
