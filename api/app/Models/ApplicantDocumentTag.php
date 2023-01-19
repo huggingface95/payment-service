e4df081033e7380a43626e7ec079f0e738a67ae0
pvsv <pvsv1984@gmail.com>
2023-01-20 01:16:30 +0200

    Add fields to applicant document tags

--- api/app/Models/ApplicantDocumentTag.php
+++ api/app/Models/ApplicantDocumentTag.php
@@ -6,16 +6,21 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ApplicantDocumentTag extends BaseModel
 {
-    public $timestamps = false;
 
     protected $fillable = [
         'category_id',
         'name',
+        'member_id',
         'description',
     ];
 
     public function category(): BelongsTo
     {
         return $this->belongsTo(ApplicantDocumentTagCategory::class, 'category_id');
+    }
+
+    public function member(): BelongsTo
+    {
+        return $this->belongsTo(Members::class, 'member_id');
     }
 }
