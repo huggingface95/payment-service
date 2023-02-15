7c2cf1b16116a31668155ce1bc9bbb01b11f398e
Arthur <arthurgevorgyan1992@gmail.com>
2023-02-15 16:59:03 +0400

    Remove company_setting table in go-auth. Change is need change password response.

--- go-auth/controllers/LoginController.go
+++ go-auth/controllers/LoginController.go
@@ -99,7 +99,7 @@ func Login(context *gin.Context) {
 
 		cache.Caching.ResetPassword.Set(randomToken, data)
 		context.JSON(http.StatusForbidden, gin.H{
-			"message": "An email has been sent to your email to confirm the password", "url": fmt.Sprintf("%s?%s", user.GetCompany().BackofficeForgotPasswordUrl, randomToken),
+			"message": "Please change password first", "url": fmt.Sprintf("%s?%s", user.GetCompany().BackofficeForgotPasswordUrl, randomToken),
 		})
 		return
 	}
