f4b110c1bf376bf906bd34226cf9ac0c25593e3c
p.vladimir <v.paryacool@gmail.com>
2023-02-16 19:48:48 +0100

    Added tests for priceListFees

--- api/tests/Feature/GraphQL/Queries/PriceListFeesQueryTest.php
+++ api/tests/Feature/GraphQL/Queries/PriceListFeesQueryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\GraphQL\Queries;
 
 use App\Models\PriceListFee;
+use App\Models\PriceListFeeCurrency;
 use Tests\TestCase;
 
 class PriceListFeesQueryTest extends TestCase
@@ -23,7 +24,9 @@ class PriceListFeesQueryTest extends TestCase
 
     public function testQueryPriceListFeesList(): void
     {
-        $priceListFees = PriceListFee::orderBy('id', 'DESC')->limit(10)->get();
+        $priceListFees = PriceListFee::orderBy('id', 'DESC')
+            ->limit(10)
+            ->get();
 
         $expect = [
             'data' => [
@@ -61,8 +64,185 @@ class PriceListFeesQueryTest extends TestCase
                 }',
             ],
             [
-                'Authorization' => 'Bearer '.$this->login(),
+                'Authorization' => 'Bearer ' . $this->login(),
             ]
         )->seeJson($expect);
+    }
+
+    /**
+     * @dataProvider provide_testQueryPriceListFeesWithFilterByCondition
+     */
+    public function testQueryPriceListFeesWithFilterByCondition($cond, $value): void
+    {
+        $priceListFees = PriceListFee::where($cond, $value)
+            ->orderBy('id', 'ASC')
+            ->get();
+
+        $expect = [
+            'data' => [
+                'priceListFees' => [],
+            ],
+        ];
+
+        foreach ($priceListFees as $priceListFee) {
+            $expect['data']['priceListFees'][] = [
+                'id' => (string) $priceListFee->id,
+                'name' => (string) $priceListFee->name,
+            ];
+        }
+
+        $this->postGraphQL(
+            [
+                'query' => 'query PriceListFees($id: Mixed) {
+                    priceListFees (
+                        filter: { column: '.strtoupper($cond).', operator: EQ, value: $id }
+                    ) {
+                            id
+                            name
+                    }
+                }',
+                'variables' => [
+                    'id' => $value,
+                ],
+            ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]
+        )->seeJsonContains($expect);
+    }
+
+    public function testQueryPriceListFeesWithFilterByCompanyId(): void
+    {
+        $priceListFees = PriceListFee::orderBy('id', 'DESC')->first();
+
+        $company = $priceListFees->company()->first();
+
+        $expect = [
+            'id' => (string) $priceListFees->id,
+            'name' => (string) $priceListFees->name,
+        ];
+
+        $this->postGraphQL(
+            [
+                'query' => 'query PriceListFees($id: Mixed) {
+                    priceListFees (
+                        filter: { column: HAS_COMPANY_FILTER_BY_ID, value: $id }
+                    ) {
+                            id
+                            name
+                    }
+                }',
+                'variables' => [
+                    'id' => $company->id,
+                ],
+            ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]
+        )->seeJsonContains($expect);
+    }
+
+    public function testQueryPriceListFeesWithFilterByPaymentProviderId(): void
+    {
+        $priceListFees = PriceListFee::orderBy('id', 'DESC')->first();
+
+        $paymentProvider = $priceListFees->paymentProvider()->first();
+
+        $expect = [
+            'id' => (string) $priceListFees->id,
+            'name' => (string) $priceListFees->name,
+        ];
+
+        $this->postGraphQL(
+            [
+                'query' => 'query PriceListFees($id: Mixed) {
+                    priceListFees (
+                        filter: { column: HAS_PAYMENT_PROVIDER_FILTER_BY_ID, value: $id }
+                    ) {
+                            id
+                            name
+                    }
+                }',
+                'variables' => [
+                    'id' => $paymentProvider->id,
+                ],
+            ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]
+        )->seeJsonContains($expect);
+    }
+
+    public function testQueryPriceListFeesWithFilterByPaymentSystemId(): void
+    {
+        $priceListFees = PriceListFee::orderBy('id', 'DESC')->first();
+
+        $paymentSystem = $priceListFees->paymentSystem()->first();
+
+        $expect = [
+            'id' => (string) $priceListFees->id,
+            'name' => (string) $priceListFees->name,
+        ];
+
+        $this->postGraphQL(
+            [
+                'query' => 'query PriceListFees($id: Mixed) {
+                    priceListFees (
+                        filter: { column: HAS_PAYMENT_SYSTEM_FILTER_BY_ID, value: $id }
+                    ) {
+                            id
+                            name
+                    }
+                }',
+                'variables' => [
+                    'id' => $paymentSystem->id,
+                ],
+            ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]
+        )->seeJsonContains($expect);
+    }
+
+    public function testQueryPriceListFeesWithFilterByFeesId(): void
+    {
+        $priceListFees = PriceListFee::orderBy('id', 'ASC')->first();
+
+        $fees = PriceListFeeCurrency::where('price_list_fee_id', $priceListFees->id)->first();
+
+        $expect = [
+            'id' => (string) $priceListFees->id,
+            'name' => (string) $priceListFees->name,
+        ];
+
+        $this->postGraphQL(
+            [
+                'query' => 'query PriceListFees($id: Mixed) {
+                    priceListFees (
+                        filter: { column: HAS_FEES_FILTER_BY_ID, value: $id }
+                    ) {
+                            id
+                            name
+                    }
+                }',
+                'variables' => [
+                    'id' => $fees->id,
+                ],
+            ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]
+        )->seeJsonContains($expect);
+    }
+
+    public function provide_testQueryPriceListFeesWithFilterByCondition()
+    {
+        return [
+            ['id', '1'],
+            ['price_list_id', '1'],
+            ['type_id', '1'],
+            ['operation_type_id', '1'],
+            ['period_id', '1'],
+        ];
     }
 }
