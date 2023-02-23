317abb41108ded913b3a87d1c1ce5cdbdc7bdc0f
p.vladimir <v.paryacool@gmail.com>
2023-02-23 18:09:50 +0100

    Added tests for applicantIndividualNotes

---
+++ api/tests/Feature/GraphQL/Mutations/ApplicantIndividualNotesMutationTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Feature\GraphQL\Mutations;
+
+use App\Models\ApplicantIndividualNotes;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class ApplicantIndividualNotesMutationTest extends TestCase
+{
+    /**
+     * ApllicantIndividualNotes Mutation Testing
+     *
+     * @return void
+     */
+    public function testCreateApplicantIndividualNoteNoAuth(): void
+    {
+        $seq = DB::table('applicant_individual_notes')
+                ->max('id') + 1;
+
+        DB::select('ALTER SEQUENCE applicant_individual_notes_id_seq RESTART WITH ' . $seq);
+
+        $this->graphQL('
+            mutation CreateApplicantIndividualNote(
+                  $note: String!
+                  $applicant_individual_id: ID!
+                  $member_id: ID!
+            ) {
+                  createApplicantIndividualNote(
+                    note: $note
+                    applicant_individual_id: $applicant_individual_id
+                    member_id: $member_id
+                  ) {
+                    id
+                    note
+                  }
+            }
+        ', [
+            'note' => 'New Note by Test',
+            'applicant_individual_id' => 1,
+            'member_id' => 2,
+        ])->seeJsonContains([
+            'message' => 'Unauthenticated.',
+        ]);
+    }
+
+    public function testCreateApplicantIndividualNote(): void
+    {
+        $seq = DB::table('applicant_individual_notes')
+                ->max('id') + 1;
+
+        DB::select('ALTER SEQUENCE applicant_individual_notes_id_seq RESTART WITH ' . $seq);
+
+        $this->postGraphQL(['query' => 'mutation CreateApplicantIndividualNote(
+                  $note: String!
+                  $applicant_individual_id: ID!
+                  $member_id: ID!
+            ) {
+                  createApplicantIndividualNote(
+                    note: $note
+                    applicant_individual_id: $applicant_individual_id
+                    member_id: $member_id
+                  ) {
+                    id
+                    note
+                  }
+            }',
+            'variables' => [
+                'note' => 'New Note by Test',
+                'applicant_individual_id' => 1,
+                'member_id' => 2,
+            ],
+        ],
+        [
+            'Authorization' => 'Bearer ' . $this->login(),
+        ]);
+
+        $id = json_decode($this->response->getContent(), true);
+
+        $this->seeJsonContains([
+            [
+                'id' => $id['data']['createApplicantIndividualNote']['id'],
+                'note' => $id['data']['createApplicantIndividualNote']['note'],
+            ],
+        ]);
+    }
+
+    public function testDeleteApplicantIndividualNote(): void
+    {
+        $applicantNote = ApplicantIndividualNotes::orderBy('id', 'DESC')->first();
+
+        $this->postGraphQL(['query' => 'mutation DeleteApplicantIndividualNote(
+                  $id: ID!
+            ) {
+                  deleteApplicantIndividualNote(
+                    id: $id
+                  ) {
+                    id
+                    note
+                  }
+            }',
+            'variables' => [
+                'id' => $applicantNote->id,
+            ],
+        ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]);
+
+        $id = json_decode($this->response->getContent(), true);
+
+        $this->seeJsonContains([
+            [
+                'id' => $id['data']['deleteApplicantIndividualNote']['id'],
+                'note' => $id['data']['deleteApplicantIndividualNote']['note'],
+            ],
+        ]);
+    }
+}
---
+++ api/tests/Feature/GraphQL/Queries/ApplicantIndividualNotesQueryTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Feature\GraphQL\Queries;
+
+use App\Models\ApplicantIndividual;
+use App\Models\ApplicantIndividualNotes;
+use Tests\TestCase;
+
+class ApplicantIndividualNotesQueryTest extends TestCase
+{
+    public function testQueryApplicantIndividualNotesNoAuth(): void
+    {
+        $this->graphQL('
+            {
+                applicantIndividualNotes {
+                    data {
+                        id
+                        note
+                    }
+                }
+            }
+        ')->seeJson([
+            'message' => 'Unauthenticated.',
+        ]);
+    }
+
+    public function testQueryApplicantIndividualNote(): void
+    {
+        $applicantNote = ApplicantIndividualNotes::orderBy('id', 'ASC')
+            ->first();
+
+        $this->postGraphQL(
+            [
+                'query' => '
+                query ApplicantIndividualNote($id: ID) {
+                    applicantIndividualNote(id: $id) {
+                            id
+                            note
+                    }
+                }',
+                'variables' => [
+                    'id' => $applicantNote->id,
+                ],
+            ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]
+        )->seeJson([
+            'data' => [
+                'applicantIndividualNote' => [
+                    'id' => (string) $applicantNote->id,
+                    'note' => (string) $applicantNote->note,
+                ],
+            ],
+        ]);
+    }
+
+    public function testQueryApplicantIndividualNotes(): void
+    {
+        $applicantNotes = ApplicantIndividualNotes::get();
+
+        foreach ($applicantNotes as $applicantNote) {
+            $data[] = [
+                'id' => (string) $applicantNote->id,
+                'note' => (string) $applicantNote->note,
+            ];
+        }
+
+        $this->postGraphQL(
+            [
+                'query' => '
+                {
+                    applicantIndividualNotes {
+                        data {
+                            id
+                            note
+                        }
+                    }
+                }',
+            ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]
+        )->seeJson([
+            'data' => [
+                'applicantIndividualNotes' => [
+                    'data' => $data,
+                ],
+            ],
+        ]);
+    }
+
+    public function testQueryApplicantIndividualNotesWithApplicantId(): void
+    {
+
+        $applicant = ApplicantIndividual::find(ApplicantIndividualNotes::select('applicant_individual_id')->first());
+
+        $applicantNotes = ApplicantIndividualNotes::where('applicant_individual_id', $applicant[0]->id)->get();
+
+        foreach ($applicantNotes as $applicantNote) {
+            $data[] = [
+                'id' => (string) $applicantNote->id,
+                'note' => (string) $applicantNote->note,
+            ];
+        }
+
+        $this->postGraphQL(
+            [
+                'query' => 'query ApplicantIndividualNotes($id: ID) {
+                    applicantIndividualNotes (
+                        applicant_individual_id: $id
+                    ) {
+                        data {
+                            id
+                            note
+                        }
+                    }
+                }',
+                'variables' => [
+                    'id' => (string) $applicant[0]->id,
+                ]
+            ],
+            [
+                'Authorization' => 'Bearer ' . $this->login(),
+            ]
+        )->seeJson([
+            'data' => [
+                'applicantIndividualNotes' => [
+                    'data' => $data,
+                ],
+            ],
+        ]);
+    }
+}
