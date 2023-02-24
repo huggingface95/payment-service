6c5e07b366ac0a2846c34d52f01c160c7a8e8865
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2023-02-24 13:35:48 +0100

    Added validation rules to fee exchange

---
+++ api/app/Rules/FeeRanges.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\ImplicitRule;
+
+class FeeRanges implements ImplicitRule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (empty($value)) {
+            return true;
+        }
+
+        foreach ($value as $feeRange) {
+            if (!isset($feeRange->fees) || !isset($feeRange->feeState)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The fee_ranges structure is not valid.';
+    }
+}
