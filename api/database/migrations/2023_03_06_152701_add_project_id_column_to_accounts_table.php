57f80857ea82610572bf22b34583113a2869927f
Arthur <arthurgevorgyan1992@gmail.com>
2023-03-06 20:23:07 +0400

    Add project_id in accounts.graphql

---
+++ api/database/migrations/2023_03_06_152701_add_project_id_column_to_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProjectIdColumnToAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_id')->nullable();
+
+            $table->foreign('project_id')->references('id')->on('projects')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn('project_id');
+        });
+    }
+}
