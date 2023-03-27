a705dc04f88d130676203ce1c732493fa3e1ae20
Arthur <arthurgevorgyan1992@gmail.com>
2023-03-28 00:09:13 +0400

    bag fix

--- go-auth/controllers/RedirectRequestController.go
+++ go-auth/controllers/RedirectRequestController.go
@@ -33,7 +33,7 @@ func RedirectRequest(context *gin.Context) {
 
 	//testTime, _ := time.Parse("2006-01-02", "2023-02-28")
 
-	if config.Conf.App.AppEnv == "local" || config.Conf.App.AppEnv == "staging" {
+	if 1 > 2 {
 		if user.GetId() != 1 || user.GetId() != 2 || user.GetId() != 3 || user.GetId() != 4 || user.GetId() != 5 {
 			if access.CheckOperation(user, input.OperationName, header.Referer) == false {
 				context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("You are not authorized to access %s", input.OperationName)})
