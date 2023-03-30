a6b5a6cb109ece459f095605017872b839be0c7e
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2023-03-30 16:09:27 +0200

    Company ledger settings

---
+++ api/app/GraphQL/Scalars/Time.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\GraphQL\Scalars;
+
+use GraphQL\Error\Error;
+use GraphQL\Type\Definition\ScalarType;
+use GraphQL\Utils\Utils;
+use Illuminate\Support\Carbon;
+
+/**
+ * Read more about scalars here https://webonyx.github.io/graphql-php/type-definitions/scalars
+ */
+final class Time extends ScalarType
+{
+    /**
+     * Serializes an internal value to include in a response.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function serialize($value)
+    {
+        return Carbon::parse($value)->format("H:i:s");
+    }
+
+    /**
+     * Parses an externally provided value (query variable) to use as an input
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function parseValue($value)
+    {
+        if (Carbon::createFromFormat('H:i:s', $value) === false) {
+            throw new Error('Cannot represent following value as Time: '.Utils::printSafeJson($value));
+        }
+
+        return $value;
+    }
+
+    /**
+     * Parses an externally provided literal value (hardcoded in GraphQL query) to use as an input.
+     *
+     * @param  \GraphQL\Language\AST\Node  $valueNode
+     * @param  array<string, mixed>|null  $variables
+     * @return mixed
+     */
+    public function parseLiteral($valueNode, ?array $variables = null)
+    {
+        if (Carbon::createFromFormat('H:i:s', $valueNode->value) === false) {
+            throw new Error('Cannot represent following value as Time: '.Utils::printSafeJson($valueNode->value));
+        }
+
+        return $valueNode->value;
+    }
+}
