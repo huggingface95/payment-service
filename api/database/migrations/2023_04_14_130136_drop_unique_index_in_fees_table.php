24a840e65898e1c67318ab3215b1c72dd044e482
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2023-04-14 15:10:38 +0200

    Fix between account transfers

---
+++ api/database/migrations/2023_04_14_130136_drop_unique_index_in_fees_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropUniqueIndexInFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fees', function (Blueprint $table) {
+            $table->dropUnique('fees_operation_type_id_price_list_fee_id_fee_type_mode_id_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fees', function (Blueprint $table) {
+            $table->unique(['operation_type_id', 'price_list_fee_id', 'fee_type_mode_id']);
+        });
+    }
+}
