af987149942ffa6c561824a02e10d38440787b6e
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2023-04-17 10:55:13 +0200

    Fix

--- api/database/seeders/QuoteProviderTableSeeder.php
+++ api/database/seeders/QuoteProviderTableSeeder.php
@@ -20,14 +20,16 @@ class QuoteProviderTableSeeder extends Seeder
         $faker = Factory::create();
 
         for ($i = 1; $i <= 10; $i++) {
-            QuoteProvider::updateOrCreate([
-                'id' => $i,
-            ], [
-                'name' => 'Quote Provider ' . $i,
-                'company_id' => $i,
-                'status' => $faker->randomElement([ActivityStatusEnum::INACTIVE->value, ActivityStatusEnum::ACTIVE->value]),
-                'quote_type' => $faker->randomElement([QuoteTypeEnum::API->toString(), QuoteTypeEnum::MANUAL->toString()]),
-            ]);
+            QuoteProvider::withoutEvents(function () use ($faker, $i) {
+                QuoteProvider::updateOrCreate([
+                    'id' => $i,
+                ], [
+                    'name' => 'Quote Provider ' . $i,
+                    'company_id' => $i,
+                    'status' => $faker->randomElement([ActivityStatusEnum::INACTIVE->value, ActivityStatusEnum::ACTIVE->value]),
+                    'quote_type' => $faker->randomElement([QuoteTypeEnum::API->toString(), QuoteTypeEnum::MANUAL->toString()]),
+                ]);
+            });
         }
     }
 }
