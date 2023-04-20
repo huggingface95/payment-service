2e5c7163a906f4377cc01ba72ebb8ff8ad1e90d9
Arthur <arthurgevorgyan1992@gmail.com>
2023-04-20 20:15:31 +0400

    Add activeCompanySessions query

--- api/app/GraphQL/Queries/ActiveSessionsQuery.php
+++ api/app/GraphQL/Queries/ActiveSessionsQuery.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Models\ApplicantCompany;
 use App\Models\ApplicantIndividual;
 use App\Models\Clickhouse\ActiveSession;
 use App\Models\Members;
@@ -22,24 +23,7 @@ final class ActiveSessionsQuery
             ->from((new ActiveSession())->getTable())
             ->orderBy('created_at', 'DESC');
 
-        if (isset($args['query']) && count($args['query']) > 0) {
-            $fields = $args['query'];
-
-            if (isset($fields['created_at'])) {
-                $value = $fields['created_at'];
-                $query->whereBetween('created_at', [substr($value['from'], 0, 10) . ' 00:00:00', substr($value['to'], 0, 10) . ' 23:59:59']);
-
-                unset($fields['created_at']);
-            }
-
-            if (count($fields) > 0) {
-                $query->where(function ($query) use ($fields) {
-                    foreach ($fields as $column => $value) {
-                        $query->where($column, $value);
-                    }
-                });
-            }
-        }
+        $this->filterByQuery($query, $args);
 
         $result = $query->paginate($args['page'] ?? 1, $args['count'] ?? env('PAGINATE_DEFAULT_COUNT'));
 
@@ -58,6 +42,47 @@ final class ActiveSessionsQuery
         ];
     }
 
+    public function getCompanySessions($_, array $args): ?array
+    {
+        $query = DB::connection('clickhouse')
+            ->query()
+            ->from((new ActiveSession())->getTable())
+            ->orderBy('created_at', 'DESC');
+
+        if (isset($args['applicant_company_id'])) {
+            /** @var ApplicantCompany $applicantCompany */
+            $applicantCompany = ApplicantCompany::query()->with('applicantIndividuals')->findOrFail($args['applicant_company_id']);
+            $query->whereIn('email', $applicantCompany->applicantIndividuals->pluck('email')->toArray());
+        }
+        if (isset($args['applicant_individual_id'])) {
+            /** @var ApplicantIndividual $individual */
+            $individual = ApplicantIndividual::query()->findOrFail($args['applicant_individual_id']);
+            $query->where('email', $individual->email);
+        }
+        if (isset($args['owner_id'])) {
+            $individuals = ApplicantIndividual::query()->where('account_manager_member_id', $args['owner_id'])->get();
+            $query->whereIn('email', $individuals->pluck('email')->toArray());
+        }
+        $this->filterByQuery($query, $args);
+
+        $result = $query->paginate($args['page'] ?? 1, $args['count'] ?? env('PAGINATE_DEFAULT_COUNT'));
+        return [
+            'data' => $result->items(),
+            'paginatorInfo' => [
+                'count' => $result->count(),
+                'currentPage' => $result->currentPage(),
+                'firstItem' => $result->firstItem(),
+                'hasMorePages' => $result->hasMorePages(),
+                'lastItem' => $result->lastItem(),
+                'lastPage' => $result->lastPage(),
+                'perPage' => $result->perPage(),
+                'total' => $result->total(),
+            ],
+        ];
+
+    }
+
+
     public function getMemberActiveSession($_, array $args): ?array
     {
         /** @var Members $member */
@@ -72,5 +97,26 @@ final class ActiveSessionsQuery
         $individual = ApplicantIndividual::query()->findOrFail($args['individual_id']);
 
         return $individual->active_session;
+    }
+
+    protected function filterByQuery(object $query, array $args): void
+    {
+        if (isset($args['query']) && count($args['query']) > 0) {
+            $fields = $args['query'];
+
+            if (isset($fields['created_at'])) {
+                $value = $fields['created_at'];
+                $query->whereBetween('created_at', [substr($value['from'], 0, 10) . ' 00:00:00', substr($value['to'], 0, 10) . ' 23:59:59']);
+
+                unset($fields['created_at']);
+            }
+            if (count($fields) > 0) {
+                $query->where(function ($query) use ($fields) {
+                    foreach ($fields as $column => $value) {
+                        $query->where($column, $value);
+                    }
+                });
+            }
+        }
     }
 }
