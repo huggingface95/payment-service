93648cd6c76d67a68c837f0f75e3280ffa89d661
pvsv <pvsv1984@gmail.com>
2023-05-04 15:18:00 +0300

    remove urgency id for outgoing transfer fee mutation

--- api/database/migrations/2022_12_02_155732_create_transfer_outgoing_table.php
+++ api/database/migrations/2022_12_02_155732_create_transfer_outgoing_table.php
@@ -4,6 +4,7 @@ use App\Enums\TransferChannelEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\PaymentUrgencyEnum;
 
 class CreateTransferOutgoingTable extends Migration
 {
@@ -22,7 +23,7 @@ class CreateTransferOutgoingTable extends Migration
             $table->decimal('amount_debt', 15, 5);
             $table->unsignedBigInteger('currency_id');
             $table->unsignedInteger('status_id');
-            $table->unsignedBigInteger('urgency_id');
+            $table->unsignedBigInteger('urgency_id')->default(PaymentUrgencyEnum::STANDART);
             $table->unsignedInteger('operation_type_id');
             $table->unsignedBigInteger('payment_provider_id');
             $table->unsignedBigInteger('payment_system_id');
