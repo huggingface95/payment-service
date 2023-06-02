1aa6a2f66b07f1974973e4ffdf80b08fb6062ccc
Arthur <arthurgevorgyan1992@gmail.com>
2023-06-02 20:02:36 +0400

    Optimize go-auth jobs

--- go-auth/repositories/redisRepository/redis.go
+++ go-auth/repositories/redisRepository/redis.go
@@ -22,6 +22,15 @@ func GetRedisDataByBlPop(key string, mc func() interface{}) interface{} {
 	return model
 }
 
+func GetRedisListByKey(key string, mc func() interface{}) (list []interface{}) {
+	for _, key := range database.GetKeys(key) {
+		redisData := GetByKey(key, mc)
+		list = append(list, redisData)
+		database.Del(key)
+	}
+	return
+}
+
 func SetRedisDataByBlPop(key string, val interface{}) bool {
 	return database.AddList(key, val)
 }
