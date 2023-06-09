9360f20024499c27101d06e03168a8e253528e45
Arthur <arthurgevorgyan1992@gmail.com>
2023-06-09 17:18:06 +0400

    bag fix

--- api/app/Models/Builders/ProjectBuilder.php
+++ api/app/Models/Builders/ProjectBuilder.php
@@ -3,14 +3,14 @@
 namespace App\Models\Builders;
 
 use App\Enums\ModuleEnum;
+use App\Models\State;
 use Illuminate\Database\Eloquent\Builder;
 
 class ProjectBuilder extends Builder
 {
     public function active($a): static
     {
-        $this->whereHas('accounts')->where('module_id', '=', ModuleEnum::BANKING);
-
+        $this->where('module_id', '=', ModuleEnum::BANKING)->where('state_id', State::ACTIVE)->whereHas('accounts');
         return $this;
     }
 }
