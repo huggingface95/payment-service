4e222ae6300a57169cc53cb4157cadf205ae0bf0
p.vladimir <v.paryacool@gmail.com>
2023-06-16 16:48:04 +0200

    Fix applicant Banking Access

--- api/app/Models/Traits/ApplicantIdPrefix.php
+++ api/app/Models/Traits/ApplicantIdPrefix.php
@@ -80,8 +80,8 @@ trait ApplicantIdPrefix
             'checks' => ['(' . ApplicantIndividual::ID_PREFIX . ')', '(' . ApplicantCompany::ID_PREFIX . ')'],
         ],
         ApplicantBankingAccess::class => [
-            'columns' => ['applicant_individual_id'],
-            'checks' => ['(' . ApplicantIndividual::ID_PREFIX . ')'],
+            'columns' => ['applicant_company_id'],
+            'checks' => ['(' . ApplicantCompany::ID_PREFIX . ')'],
         ],
         KycTimeline::class => [
             'columns' => ['applicant_id'],
