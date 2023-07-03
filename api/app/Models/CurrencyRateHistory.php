c173e2eb4f5c57b2d6c77934715f0908c0c63d54
p.vladimir <v.paryacool@gmail.com>
2023-07-03 16:25:45 +0200

    Added queries rates and historyRates

--- api/app/Models/CurrencyRateHistory.php
+++ api/app/Models/CurrencyRateHistory.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Traits\BaseObServerTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CurrencyRateHistory extends BaseModel
 {
@@ -25,4 +26,19 @@ class CurrencyRateHistory extends BaseModel
         'rate' => 'decimal:5',
         'created_at' => 'datetime:YYYY-MM-DDTHH:mm:ss.SSSZ',
     ];
+
+    public function quoteProvider(): BelongsTo
+    {
+        return $this->belongsTo(QuoteProvider::class, 'quote_provider_id');
+    }
+
+    public function currencyFrom(): BelongsTo
+    {
+        return $this->belongsTo(Currencies::class, 'currency_src_id');
+    }
+
+    public function currencyTo(): BelongsTo
+    {
+        return $this->belongsTo(Currencies::class, 'currency_dst_id');
+    }
 }
