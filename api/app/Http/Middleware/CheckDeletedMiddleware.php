c8ef7ce6861b92ad42cdcf6ba1b655c5f712fcc0
p.vladimir <v.paryacool@gmail.com>
2023-07-17 12:04:08 +0200

    Added check when deleting soft deleted records

---
+++ api/app/Http/Middleware/CheckDeletedMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\GraphqlException;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CheckDeletedMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response->status() === 200 && $request->path() === 'api') {
+            $responseData = json_decode($response->getContent(), true);
+
+            foreach ($responseData['data'] as $mutationName => $mutationResult) {
+                if ($mutationResult === null && Str::startsWith($mutationName, 'delete')) {
+                    throw new GraphqlException('An entry not found', 'internal', 404);
+                }
+            }
+        }
+
+        return $response;
+    }
+}
