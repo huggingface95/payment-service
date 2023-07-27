7740688f8790894b5d542ef7018e2a716236de75
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2023-07-27 12:09:13 +0200

    Changed OWT fee

--- api/app/DTO/Transfer/Create/Outgoing/CreateTransferOutgoingFeeDTO.php
+++ api/app/DTO/Transfer/Create/Outgoing/CreateTransferOutgoingFeeDTO.php
@@ -4,11 +4,11 @@ namespace App\DTO\Transfer\Create\Outgoing;
 
 use App\Enums\PaymentStatusEnum;
 use App\Enums\PaymentUrgencyEnum;
+use App\Enums\RespondentFeesEnum;
 use App\Enums\TransferChannelEnum;
 use App\Exceptions\GraphqlException;
 use App\Models\Account;
 use App\Models\Company;
-use App\Models\PaymentBank;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 
@@ -24,17 +24,15 @@ class CreateTransferOutgoingFeeDTO extends CreateTransferOutgoingDTO
 
         $date = Carbon::now()->format('Y-m-d H:i:s');
         $args['amount_debt'] = $args['amount'];
-        $args['currency_id'] = $account->currency_id;
+        $args['currency_id'] = $args['currency_id'] == $account->currency_id ? $args['currency_id'] : throw new GraphqlException('The account has a different currency', 'use');
         $args['status_id'] = PaymentStatusEnum::UNSIGNED->value;
         $args['operation_type_id'] = $operationType;
-        $args['payment_bank_id'] = PaymentBank::query()->where('payment_provider_id', $args['payment_provider_id'])->where('payment_system_id', $args['payment_system_id'])->first()?->id ?? throw new GraphqlException('Payment bank not found', 'use');
+        $args['payment_bank_id'] = null;
         $args['payment_number'] = Str::uuid();
         $args['system_message'] = '';
         $args['channel'] = TransferChannelEnum::BACK_OFFICE->toString();
         $args['urgency_id'] ??= PaymentUrgencyEnum::STANDART->value;
         $args['created_at'] = $date;
-        $args['recipient_bank_country_id'] ??= Company::findOrFail($args['company_id'])->country_id;
-        $args['recipient_country_id'] ??= Company::findOrFail($args['company_id'])->country_id;
         $args['project_id'] = $account->project_id;
 
         if (!empty($args['execution_at'])) {
@@ -47,6 +45,11 @@ class CreateTransferOutgoingFeeDTO extends CreateTransferOutgoingDTO
         } else {
             $args['execution_at'] = $args['created_at'];
         }
+
+        /* @todo: Check this fields */
+        $args['recipient_bank_country_id'] ??= Company::findOrFail($args['company_id'])->country_id;
+        $args['recipient_country_id'] ??= Company::findOrFail($args['company_id'])->country_id;
+        $args['respondent_fees_id'] ??= RespondentFeesEnum::CHARGED_TO_CUSTOMER->value;
 
         return new parent($args, $account);
     }
